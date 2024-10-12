5c07a654ddd5fbb1ea6102d4e134ad45702c41e5
Umair Inayat <68996836+umairinayat@users.noreply.github.com>
2024-10-13 04:07:14 +0500

    Time: 280 ms (62.65%) | Memory: 114.6 MB (46.53%) - LeetSync

---
+++ 2488-divide-intervals-into-minimum-number-of-groups/divide-intervals-into-minimum-number-of-groups.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        vector<pair<int,int>> st;
+
+        for(auto it : intervals){
+            st.push_back({it[0],1});
+            st.push_back({it[1]+1,-1});
+        }
+
+        sort(st.begin(),st.end());
+
+        int maxi = 1;
+        int grp = 0;
+
+        for(auto it : st){
+            grp += it.second;
+            maxi = max(grp,maxi);
+        }
+
+        return maxi;
+    }
+};
