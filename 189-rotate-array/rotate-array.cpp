c0683342c24f72184cd1f847b6d465437853bbc3
Umair Inayat <68996836+umairinayat@users.noreply.github.com>
2024-10-19 12:35:51 +0500

    Time: 0 ms (100.00%) | Memory: 27.6 MB (50.15%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,5 +1,12 @@
 class Solution {
 public:
+    /*Approach
+Normalize Rotation: Compute the effective number of rotations needed by taking k % nums.size() to handle cases where k is larger than the array length.
+Reverse Subarrays:
+Reverse the first part of the array, from the beginning to the end minus k elements.
+Reverse the second part of the array, from the end minus k elements to the end.
+Reverse the entire array to achieve the final rotated result.
+Combine Results: The combined effect of these three reversals results in the array being rotated to the right by k positions.*/
     void rotate(vector<int>& nums, int k) {
          k=k%nums.size();
         reverse(nums.begin(),nums.begin()+(nums.size()-k));
