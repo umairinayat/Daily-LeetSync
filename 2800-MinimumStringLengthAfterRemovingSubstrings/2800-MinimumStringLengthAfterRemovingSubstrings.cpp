1b078ffaaae44eba53ff1c956ddd620f4083cc42
Umair Inayat <68996836+umairinayat@users.noreply.github.com>
2025-12-23 00:15:06 +0500

    Create 2800-MinimumStringLengthAfterRemovingSubstrings

---
+++ 2800-MinimumStringLengthAfterRemovingSubstrings/2800-MinimumStringLengthAfterRemovingSubstrings.cpp
@@ -0,0 +1,28 @@
+// Last updated: 23/12/2025, 00:14:58
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> stack;
+
+        for (int i = 0; i < s.length(); i++) {
+            char cur_char = s[i];
+
+            if (stack.empty()) {
+                stack.push(cur_char);
+                continue;
+            }
+
+            if (cur_char == 'B' && stack.top() == 'A') {
+                stack.pop();
+            }
+            else if (cur_char == 'D' && stack.top() == 'C') {
+                stack.pop();
+            }
+            else {
+                stack.push(cur_char);
+            }
+        }
+
+        return stack.size();
+    }
+};
